38499ecf0648ba2c4f9510f27e442d1dfeaa7e33
Yii.Guxing <yii.guxing@outlook.com>
2023-03-14 01:56:43 +0800

    `UpdateInBackgroundCompatAction` compatible to an exact version

--- src/main/kotlin/cn/yiiguxing/intellij/compat/action/UpdateInBackgroundCompatAction.kt
+++ src/main/kotlin/cn/yiiguxing/intellij/compat/action/UpdateInBackgroundCompatAction.kt
@@ -1,13 +1,17 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "UnstableApiUsage")
 
 package cn.yiiguxing.intellij.compat.action
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.UpdateSession
+import com.intellij.openapi.actionSystem.impl.Utils
 import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.util.BuildNumber
 import com.intellij.openapi.util.NlsActions
 import com.intellij.util.ui.EDT
+import org.jetbrains.annotations.ApiStatus
 import java.util.function.Supplier
 import javax.swing.Icon
 
@@ -34,20 +38,36 @@ abstract class UpdateInBackgroundCompatAction : AnAction {
         icon: Icon?
     ) : super(dynamicText, dynamicDescription, icon)
 
+
+    @ApiStatus.AvailableSince("222.3345.118")
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.BGT
     }
 
+    @ApiStatus.AvailableSince("222.3345.118")
     protected fun <T> computeOnEDTUpdateSession(
         event: AnActionEvent,
         operationName: String,
         supplier: Supplier<out T>
     ): T {
-        if (ApplicationInfo.getInstance().build.baselineVersion >= 223) {
-            return event.updateSession.compute(this, operationName, ActionUpdateThread.EDT, supplier)
+        val build = ApplicationInfo.getInstance().build
+        if (build >= BGT_SINCE_VERSION) {
+            // Will not return null since 223.8617
+            var updateSession: UpdateSession? = event.updateSession
+            @Suppress("KotlinConstantConditions")
+            if (updateSession == null) {
+                @Suppress("DEPRECATION", "removal")
+                updateSession = Utils.getOrCreateUpdateSession(event)
+            }
+
+            return updateSession.compute(this, operationName, ActionUpdateThread.EDT, supplier)
         }
 
         EDT.assertIsEdt()
         return supplier.get()
+    }
+
+    companion object {
+        private val BGT_SINCE_VERSION = BuildNumber("", 222, 3345, 118)
     }
 }
