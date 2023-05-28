13b7c148f57c1b4337e2b91d67c76deae12861a6
Yii.Guxing <yii.guxing@outlook.com>
2023-05-28 20:12:38 +0800

    Bump org.jetbrains.kotlin.jvm from 1.8.20 to 1.8.21 Bump org.jetbrains.intellij from 1.13.2 to 1.13.3

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.13.2"
+    id("org.jetbrains.intellij") version "1.13.3"
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
